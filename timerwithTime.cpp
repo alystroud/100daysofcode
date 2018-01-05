0b8b278c3d32759bf42fd1fbd91a6f5e81fd6810
alystroud <alygrace@hotmail.co.uk>
2018-01-05 19:36:36 +0000

    #100daysofcode D5: Using input from command line and putting a for loop in

--- timerwithTime.cpp
+++ timerwithTime.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <chrono>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
-int main(void) {
+int main(int argc, char ** args) {
+    int repetitions = 0;
+    try{
+        repetitions = stoi(args[1]);
+    }catch(exception e){
+        cout << args[0] << endl;
+        cout << "Input code: ./timerwithTime 3" << endl;
+        return 1;
+    }
+
     auto startTime = chrono::system_clock::now();
     std::time_t start_time = std::chrono::system_clock::to_time_t(startTime);
     cout << ctime(&start_time);
 
-    bool finished = false;
+    for(auto i = 0; i < repetitions; i++){
+        bool finished = false;
     while(!finished) {
         auto currentTime = chrono::system_clock::now();
         double elapsedTime = chrono::system_clock::to_time_t(currentTime)-chrono::system_clock::to_time_t(startTime);
         if(elapsedTime >= 60){
             std::time_t end_time = std::chrono::system_clock::to_time_t(currentTime);
             cout << ctime(&end_time);
+            cout << (i+1) << " minutes gone." << endl;
             finished = true;
         }
+    }
+    startTime = chrono::system_clock::now();
+    std::time_t start_time = std::chrono::system_clock::to_time_t(startTime);
+
     }
 }
