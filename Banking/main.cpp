2342893a387a15ce5105a082e283ff41c44b209d
alystroud <alygrace@hotmail.co.uk>
2018-01-12 22:22:40 +0000

    #100DaysOfCode D12: Added * for passwords and loop for option so it doesn't end before the user chooses to exit

--- Banking/main.cpp
+++ Banking/main.cpp
@@ -1,6 +1,27 @@
 #include "User.h"
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int getch() {
+    int ch;
+    struct termios t_old, t_new;
+
+    tcgetattr(STDIN_FILENO, &t_old);
+    t_new = t_old;
+    t_new.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &t_new);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
+    return ch;
+}
 
 void start(){
+    const char BACKSPACE=127;
+    const char RETURN=10;
     User* loggedinUser;
     int option = 0;
     User userone(0, "John Doe", "jdoe1", "password1");
@@ -9,18 +30,27 @@ void start(){
     cout << endl <<  "Welcome to X Bank" << endl;
     cout << "----------------------" << endl;
     cout << "Enter your username to log in." << endl;
-    string inputUsername, inputPassword;
+    string inputUsername;
     cin >> inputUsername; 
     cout << "Password: ";
-    cin >> inputPassword;
+    int ch = getch();
+    string inputPassword = "";
+    while((ch = getch()) != RETURN) {
+        cout << "*";
+        inputPassword += ch;
+    }
+    cout << endl;
+    //cin >> inputPassword;
     if(userone.getUsername() == (inputUsername) && 
         userone.getPassword() == (inputPassword)){
             loggedinUser = &userone;
             cout << "Welcome " << userone.getName() << endl << endl;
+            while(option != 4){
             cout << "Choose an option: " << endl;
             cout << "1. View account balance" << endl;
             cout << "2. Deposit funds" << endl;
             cout << "3. Withdraw funds" << endl;
+            cout << "4. Exit" << endl;
             cin >> option;
             if(option == 1){
                 cout << "Account balance: £" << loggedinUser->getBalance() << endl;
@@ -35,11 +65,12 @@ void start(){
                 cout << "How much would you like to withdraw?" << endl;
                 float amount;
                 cin >> amount;
-                loggedinUser->deposit(amount);
+                loggedinUser->withdraw(amount);
             }
             else {
                 cout << "Error. Please choose one of the options" << endl;
             }
+        }    
     }
 }
 
@@ -48,3 +79,5 @@ int main(int argc, char** argv){
     start();
     return 0;
 }
+
+
